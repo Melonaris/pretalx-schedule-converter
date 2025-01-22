cb9e55ec453b4e54a3d2c7919e2b101ac3db2251
Benedikt Schwiede <benedikt.schwiede@protonmail.ch>
2025-01-22 13:48:54 +0100

    added timeformat field to Settings

--- src/main/java/ch/melonaris/Settings.java
+++ src/main/java/ch/melonaris/Settings.java
@@ -4,4 +4,5 @@ import java.util.Locale;
 
 public class Settings {
     static Locale localLanguage = new Locale.Builder().setLanguage("en").setRegion("US").build();
+    static TimeFormat timeFormat = TimeFormat.MILITARY;
 }
